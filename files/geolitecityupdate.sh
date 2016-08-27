568ef4eef467f67c37885d3be8340b62bc17d550
Geoffrey Hughes <geoffrey.hughes@otago.ac.nz>
2016-08-27 16:43:37 +1200

    Fix GeoIPCity.dat filename

--- files/geolitecityupdate.sh
+++ files/geolitecityupdate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /usr/bin/curl -s "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz" \
-    | /usr/bin/gunzip -c > /usr/share/GeoIP/GeoLiteCity.dat
+    | /usr/bin/gunzip -c > /usr/share/GeoIP/GeoIPCity.dat
